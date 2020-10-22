cf667c705698a5787ea0e9c4713de843e1e72c5e
ivakhnenkoalex <you@example.comivakhnenko_alex@bk.ru>
2020-10-22 12:13:16 +0300

    ОП

--- ft_count_words.py
+++ ft_count_words.py
@@ -9,9 +9,16 @@ def ft_count_words(str):
     k = 0
     d = ft_len(str)
     i = 0
-    while i < d:
-        if str[i] == ' ' and str[i - 1] != ' ':
-            k = k + 1
-        i = i + 1
-    k = k
-    return k
+    if str[0] == ' ':
+        while i < d:
+            if str[i] == ' ' and str[i - 1] != ' ':
+                k = k + 1
+            i = i + 1
+        return k
+    else:
+        while i < d:
+            if str[i] == ' ' and str[i - 1] != ' ':
+                k = k + 1
+            i = i + 1
+        return k + 1
+print(ft_count_words('f j'))
