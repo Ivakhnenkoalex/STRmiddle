c0f8bf765391a8fbf33e4f1ba8f02e6eaa2adf99
ivakhnenkoalex <you@example.comivakhnenko_alex@bk.ru>
2020-10-22 12:42:28 +0300

    GHJ

--- ft_change_str.py
+++ ft_change_str.py
@@ -1,7 +1,18 @@
-def ft_fmax(a,b):
-    if a > b:
-        return a
+def ft_len(stsr):
+    l = 0
+    for i in stsr:
+        l += 1
+    return l
+
+
+def ft_change_str(str1, str2, str3):
+    a = ''
+    ind = 0
+    if str3.find(str1) == -1:
+        return False
     else:
-        return b
-
-
+        while str3.find(str1) != -1:
+            ind = str3.find(str1)
+            a = a + str3[:ind] + str2
+            str3 = str3[ind + ft_len(str1):]
+    return a + str3
